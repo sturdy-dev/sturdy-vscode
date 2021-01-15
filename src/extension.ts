e0c75640041899a20ebb7525c467a4f27d0c7cb0
Kiril Videlov <kiril@videlov.com>
2021-01-15 13:55:48 +0100

    Playing with git

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,12 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
+import simpleGit, {
+  SimpleGit,
+  SimpleGitOptions,
+  TaskOptions,
+} from "simple-git";
+import { format } from "path";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -15,11 +21,40 @@ export function activate(context: vscode.ExtensionContext) {
   let disposable = vscode.commands.registerCommand("sturdy.helloWorld", () => {
     // The code you place here will be executed every time your command is executed
 
+    foo();
     // Display a message box to the user
-    vscode.window.showInformationMessage("Hello World from Sturdy!");
+    vscode.window.showInformationMessage("Hello Sturdy again!");
   });
 
   context.subscriptions.push(disposable);
+}
+
+function init(): SimpleGit {
+  const options: SimpleGitOptions = {
+    baseDir: process.cwd(),
+    binary: "git",
+    maxConcurrentProcesses: 6,
+  };
+  const git: SimpleGit = simpleGit(options);
+  return git;
+}
+
+function foo() {
+  const options: SimpleGitOptions = {
+    baseDir: process.cwd(),
+    binary: "git",
+    maxConcurrentProcesses: 6,
+  };
+  const git: SimpleGit = simpleGit(options);
+  git.addRemote("check-conflicts", "~/tmp/mockremote");
+  git.branch().then((br: any) => {
+    let currentBranch = br.current;
+    git.push("check-conflicts", currentBranch);
+  });
+  // git.log().then((x: any) => console.log(x));
+  // const taskOptions: TaskOptions = {};
+  // let rsp = await git.log(taskOptions, (x: any) => console.log(x));
+  // console.log(rsp);
 }
 
 // this method is called when your extension is deactivated
